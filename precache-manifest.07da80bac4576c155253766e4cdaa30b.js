a3f0f8da114ed887e629f06d49af5cffd7d8c247
Alain Guillon <zyrass@outlook.fr>
2020-01-22 13:06:19 +0100

    push project

---
+++ precache-manifest.07da80bac4576c155253766e4cdaa30b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ded2f2bfe8f361d203f5537d9c84075",
+    "url": "/index.html"
+  },
+  {
+    "revision": "da3702872933f1e33135",
+    "url": "/static/css/main.7835ea85.chunk.css"
+  },
+  {
+    "revision": "457f3b1b06b465a5ee91",
+    "url": "/static/js/2.b3a0c0aa.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.b3a0c0aa.chunk.js.LICENSE"
+  },
+  {
+    "revision": "da3702872933f1e33135",
+    "url": "/static/js/main.49c1d8e5.chunk.js"
+  },
+  {
+    "revision": "2abd1bc94832649a51a7",
+    "url": "/static/js/runtime-main.51635d2f.js"
+  }
+]);
